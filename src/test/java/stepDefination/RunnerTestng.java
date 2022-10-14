fed9dd0314549827d4b47d92777c12597f3eb493
Manish.Saha <Manish.Saha@AQMIT-LPT-10403>
2022-10-14 13:24:14 +0530

    Parallel Test commit

--- src/test/java/stepDefination/RunnerTestng.java
+++ src/test/java/stepDefination/RunnerTestng.java
@@ -1,9 +1,13 @@
 package stepDefination;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+
 import org.testng.annotations.Test;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+
 
 @Test
 @CucumberOptions(
@@ -11,11 +15,15 @@ import io.cucumber.testng.CucumberOptions;
 		glue="stepDefination",
 		dryRun = false,
 		monochrome = true,
-		tags = "@tag1",
+//		tags = "@tag2",
 		plugin = {"json:Reports/Testng/Cucumber.json","html:Reports/Testng/Cucumber.html"}
 
 		)
 public class RunnerTestng extends AbstractTestNGCucumberTests{
 
-	
+	@Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
